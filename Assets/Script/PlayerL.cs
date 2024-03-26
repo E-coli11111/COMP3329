eb085ed056e015585b54ac5faf7ad86288992b88
E-coli11111 <u3577240@connect.hku.hk>
2024-03-26 20:45:10 +0800

    update

--- Assets/Script/PlayerL.cs
+++ Assets/Script/PlayerL.cs
@@ -2,32 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerLeft : MonoBehaviour
 {
     float speed = 10;
     public Rigidbody2D rb;
     public Vector2 v;
+    public Transform t;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        t = GetComponent<Transform>();
         v = rb.velocity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.S))
         {
+
             v.y = -speed;
             rb.velocity = v;
+
         }
 
-        else if (Input.GetKey(KeyCode.UpArrow))
+        else if (Input.GetKey(KeyCode.W))
         {
+
             v.y = speed;
             rb.velocity = v;
+
         }
 
         else
---
+++ Assets/Script/PlayerR.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRight : MonoBehaviour
+{
+    float speed = 10;
+    public Rigidbody2D rb;
+    public Vector2 v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        v = rb.velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            v.y = -speed;
+            rb.velocity = v;
+        }
+
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            v.y = speed;
+            rb.velocity = v;
+        }
+
+        else
+        {
+            v.y = 0;
+            rb.velocity = v;
+
+        }
+        v.x = 0;
+        rb.velocity = v;
+    }
+}

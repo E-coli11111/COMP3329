b2305bf5c5190c815ff7f4645057e1a8f54c6066
E-coli11111 <u3577240@connect.hku.hk>
2024-04-25 13:57:46 +0800

    Update

--- Assets/Script/StartMenu.cs
+++ Assets/Script/StartMenu.cs
@@ -6,6 +6,13 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject g;
+
+    void Start()
+    {
+        // rb = GetComponent<Rigidbody2D>();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -16,9 +23,20 @@ public class StartMenu : MonoBehaviour
         Application.Quit();
     }
 
+    public void GrenadeMove()
+    {
+        Debug.Log("GrenadeMove");
+        Debug.Log(g.name);
+        Debug.Log(g.GetComponent<Rigidbody2D>().velocity);
+        g.GetComponent<Rigidbody2D>().velocity = g.GetComponent<Rigidbody2D>().velocity + new Vector2(Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+        Debug.Log(g.GetComponent<Rigidbody2D>().velocity);
+    }
     // Update is called once per frame
-    // void Update()
-    // {
-        
-    // }
+    void Update()
+    {
+        // if (Input.GetButton("Fire1"))
+        // {
+            
+        // }
+    }
 }
